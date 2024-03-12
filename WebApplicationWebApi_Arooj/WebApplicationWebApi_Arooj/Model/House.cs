035cb500c1b11af2e347f352c04adf0f039257ce
Arooj448 <aroojali448@gmail.com>
2024-03-12 15:53:41 +0000

    housemodelchangings

--- WebApplicationWebApi_Arooj/WebApplicationWebApi_Arooj/Model/House.cs
+++ WebApplicationWebApi_Arooj/WebApplicationWebApi_Arooj/Model/House.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string? Title { get; set; }
-        public string? Description { get; set; }
+        public string? Description { get; set; }  //house description
         public string? Images { get; set; } // You can store image URLs or paths
         public decimal Rent { get; set; }
         public bool IsAvailable { get; set; }
